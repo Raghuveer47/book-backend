9239b1a619c2b58b031f58d1c52375dde76dcd8f
Raghuveer47 <122000888+Raghuveer47@users.noreply.github.com>
2023-06-22 14:08:41 +0530

    Delete client directory

--- client/src/components/Jumbotron/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function Jumbotron({ children }) {
-  return <div className="jumbotron mt-4 bg-dark text-white">{children}</div>;
-}
-
-export default Jumbotron;
